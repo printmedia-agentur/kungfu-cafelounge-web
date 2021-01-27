a85637d5f9414dadf8f4a302ce1d109494ac45be
Domenik Töfflinger <domenikt96@yahoo.de>
2021-01-27 17:37:16 +0100

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './templates/main/main.component';
+import { HomeComponent } from './templates/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'app',
+  component: MainComponent,
+  children: [
+    {
+      path: 'home',
+      component: HomeComponent
+    }]
+  },
+{
+    path: '',
+    component: HomeComponent
+},
+{
+    path: '**',
+    component: HomeComponent
+},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
